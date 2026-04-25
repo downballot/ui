7d7537f1733c00494ae5f72813c49cb2b893b0c7
Douglas Danger Manley <doug.manley@gmail.com>
2026-04-25 10:54:16 -0400

    API wrapper

---
+++ go-ui/api/api.go
@@ -0,0 +1,33 @@
+package api
+
+import (
+	"errors"
+	"log/slog"
+
+	"github.com/downballot/downballot/downballotapi"
+	"github.com/maxence-charriere/go-app/v10/pkg/app"
+	"github.com/tekkamanendless/httperror"
+)
+
+// Do an API request, navigating to the login page if the user is not logged in
+// or if her token is invalid.
+func Do(ctx app.Context, method string, path string, input any, output any) error {
+	var apiToken string
+	ctx.GetState("api-token", &apiToken)
+	slog.InfoContext(ctx.Context, "API wrapper; state", "api-token", apiToken)
+	if apiToken != "" {
+		slog.InfoContext(ctx.Context, "API wrapper; user is not logged in (no token)")
+		ctx.Navigate("/login")
+	}
+
+	client := downballotapi.New("/")
+	err := client.Do(ctx.Context, method, path, input, output)
+	if err != nil {
+		if errors.Is(err, httperror.ErrStatusUnauthorized) {
+			slog.InfoContext(ctx.Context, "API wrapper; user is not logged in: %v", err)
+			ctx.Navigate("/login")
+		}
+		return err
+	}
+	return nil
+}
--- go-ui/go.mod
+++ go-ui/go.mod
@@ -5,12 +5,12 @@ go 1.26.2
 require (
 	github.com/downballot/downballot v0.0.0-00010101000000-000000000000
 	github.com/maxence-charriere/go-app/v10 v10.1.11
+	github.com/tekkamanendless/httperror v1.0.1
 )
 
 require (
 	github.com/emicklei/go-restful/v3 v3.13.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
-	github.com/tekkamanendless/httperror v1.0.1 // indirect
 	github.com/tekkamanendless/restapiclient v0.1.1 // indirect
 )
 
