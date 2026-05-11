281e49ebb436e26f6a059e4bdc6aae553294ae7e
Douglas Danger Manley <doug.manley@gmail.com>
2026-05-11 00:03:28 -0400

    Type

--- component/page/organization-id-person-field-page.go
+++ component/page/organization-id-person-field-page.go
@@ -94,6 +94,12 @@ func (c *OrganizationIDPersonFieldPage) Render() app.UI {
 							},
 						},
 						{
+							Name: "Type",
+							Value: func(row *downballotapi.PersonField) any {
+								return row.Type
+							},
+						},
+						{
 							Name: "Allow Empty",
 							Value: func(row *downballotapi.PersonField) any {
 								return row.AllowEmpty
