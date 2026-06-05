8a110985669cb8030e837df50e130f382dd1bd99
Douglas Danger Manley <doug.manley@gmail.com>
2026-06-05 14:21:37 -0400

    User/group management

---
+++ component/add-user-to-group-dialog.go
@@ -0,0 +1,130 @@
+package component
+
+import (
+	"context"
+	"log/slog"
+	"net/http"
+
+	"github.com/downballot/downballot/downballotapi"
+	"github.com/downballot/ui/api"
+	"github.com/downballot/ui/myui"
+	"github.com/maxence-charriere/go-app/v11/pkg/app"
+)
+
+type AddUserToGroupDialog struct {
+	app.Compo
+
+	OrganizationID string
+	UserID         string
+	OnSuccess      func(ctx app.Context)
+
+	DialogID string
+	error    string
+
+	groups  []*downballotapi.Group
+	groupID string
+	owner   bool
+}
+
+func (c *AddUserToGroupDialog) Open(ctx app.Context) {
+	slog.InfoContext(ctx.Context, "AddUserToGroupDialog: Open", "DialogID", c.DialogID)
+	slog.InfoContext(ctx.Context, "AddUserToGroupDialog: Open", "JSValue", c.JSValue(), "JSValue", app.Window().Get("JSON").Call("stringify", c.JSValue()))
+
+	dialogElement := app.Window().GetElementByID(c.DialogID)
+	if dialogElement == nil || dialogElement.IsNull() {
+		slog.ErrorContext(context.TODO(), "Could not get dialog element", "dialogID", c.DialogID)
+		return
+	}
+	dialogElement.Call("showModal")
+
+	ctx.Async(func() {
+		var output downballotapi.ListGroupsResponse
+		err := api.Do(ctx, http.MethodGet, "/api/v1/organization/"+c.OrganizationID+"/group", nil, &output)
+		if err != nil {
+			slog.ErrorContext(ctx.Context, "Could not get groups", "err", err)
+			return
+		}
+
+		ctx.Dispatch(func(ctx app.Context) {
+			slog.InfoContext(ctx.Context, "Dispatch: Setting groups", "groups", output.Groups)
+			c.groups = output.Groups
+
+			// TODO: WE NEED A TABLE-LIKE BIND THING HERE SO THAT THE PARENT CAN BE UPDATED.
+
+			ctx.Update()
+		})
+	})
+}
+
+func (c *AddUserToGroupDialog) Close(ctx app.Context) {
+	dialogElement := app.Window().GetElementByID(c.DialogID)
+	if dialogElement == nil || dialogElement.IsNull() {
+		slog.ErrorContext(context.TODO(), "Could not get dialog element", "dialogID", c.DialogID)
+		return
+	}
+	dialogElement.Call("close")
+}
+
+func (c *AddUserToGroupDialog) Render() app.UI {
+	slog.InfoContext(context.TODO(), "AddUserToGroupDialog: Render", "OrganizationID", c.OrganizationID, "UserID", c.UserID, "groups", len(c.groups))
+
+	return app.Dialog().
+		ID(c.DialogID).
+		Body(
+			app.H2().Text("Add User To Group"),
+			app.Div().
+				Body(
+					myui.Select().
+						Name("group_id").
+						Label("Group").
+						AllowedValue(
+							func() []myui.SelectOption {
+								var allowedValues []myui.SelectOption
+								for _, group := range c.groups {
+									allowedValues = append(allowedValues, myui.SelectOption{Label: group.Name, Value: group.ID})
+								}
+								return allowedValues
+							}()...).
+						Bind(&c.groupID),
+					myui.Input[bool]().
+						Label("Owner").
+						Bind(&c.owner),
+				),
+			app.If(c.error != "", func() app.UI {
+				return myui.StatusBar().
+					Text(c.error).
+					Bad()
+			}),
+			app.Div().
+				Class("myui-dialog-actions").
+				Body(
+					myui.Button().
+						Label("Cancel").
+						On("click", func(ctx app.Context, event app.Event) {
+							c.Close(ctx)
+						}),
+					app.Span().Style("flex", "1"),
+					myui.Button().
+						Label("Add").
+						On("click", func(ctx app.Context, event app.Event) {
+							ctx.Async(func() {
+								input := downballotapi.AddUserToGroupRequest{
+									GroupID: c.groupID,
+									Owner:   c.owner,
+								}
+								var output downballotapi.AddUserToGroupResponse
+								err := api.Do(ctx, http.MethodPost, "/api/v1/organization/"+c.OrganizationID+"/user/"+c.UserID+"/group", input, &output)
+								if err != nil {
+									slog.ErrorContext(ctx.Context, "Could not add user to group", "err", err, "input", input, "output", output)
+									return
+								}
+								c.Close(ctx)
+
+								if c.OnSuccess != nil {
+									c.OnSuccess(ctx)
+								}
+							})
+						}),
+				),
+		)
+}
--- component/page/organization-id-user-id-page.go
+++ component/page/organization-id-user-id-page.go
@@ -2,14 +2,17 @@ package page
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
 	"net/http"
 	"sync"
 
 	"github.com/downballot/downballot/downballotapi"
 	"github.com/downballot/ui/api"
+	"github.com/downballot/ui/component"
 	"github.com/downballot/ui/myui"
 	"github.com/go-app-blazar/router"
+	"github.com/google/uuid"
 	"github.com/maxence-charriere/go-app/v11/pkg/app"
 )
 
@@ -23,7 +26,9 @@ type OrganizationIDUserIDPage struct {
 	userID         string
 	user           *downballotapi.User
 
-	groups []*downballotapi.Group
+	userGroups []*downballotapi.UserGroup
+
+	addUserToGroupDialog component.AddUserToGroupDialog
 }
 
 var _ app.Navigator = (*OrganizationIDUserIDPage)(nil)
@@ -37,6 +42,19 @@ func (c *OrganizationIDUserIDPage) OnNav(ctx app.Context) {
 	slog.InfoContext(ctx.Context, "OrganizationIDUserIDPage: OnNav", "OrganizationID", c.organizationID)
 	slog.InfoContext(ctx.Context, "OrganizationIDUserIDPage: OnNav", "UserID", c.userID)
 
+	c.addUserToGroupDialog = component.AddUserToGroupDialog{
+		OrganizationID: c.organizationID,
+		UserID:         c.userID,
+		DialogID:       "id-" + uuid.New().String(),
+		OnSuccess: func(ctx app.Context) {
+			c.Reload(ctx)
+		},
+	}
+
+	c.Reload(ctx)
+}
+
+func (c *OrganizationIDUserIDPage) Reload(ctx app.Context) {
 	if c.organizationID == "" {
 		return
 	}
@@ -68,7 +86,19 @@ func (c *OrganizationIDUserIDPage) OnNav(ctx app.Context) {
 				c.user = output.User
 			})
 		})
-		// TODO: GET THE GROUPS FOR THE USER
+		wg.Go(func() {
+			var output downballotapi.ListUserGroupsResponse
+			err := api.Do(ctx, http.MethodGet, "/api/v1/organization/"+c.organizationID+"/user/"+c.userID+"/group", nil, &output)
+			if err != nil {
+				slog.ErrorContext(ctx.Context, "Could not get groups", "err", err)
+				return
+			}
+
+			ctx.Dispatch(func(ctx app.Context) {
+				slog.InfoContext(ctx.Context, "Dispatch: Setting user groups", "user groups", output.UserGroups)
+				c.userGroups = output.UserGroups
+			})
+		})
 		wg.Wait()
 
 		ctx.Dispatch(func(ctx app.Context) {
@@ -99,5 +129,62 @@ func (c *OrganizationIDUserIDPage) Render() app.UI {
 					app.Div().Text("Name: "+c.user.Name),
 					app.Div().Text("E-mail address: "+c.user.Username),
 				),
+			myui.Table[*downballotapi.UserGroup]().
+				Rows(c.userGroups).
+				Columns([]myui.TableColumn[*downballotapi.UserGroup]{
+					{
+						Name: "ID",
+						Value: func(row *downballotapi.UserGroup) any {
+							return row.ID
+						},
+					},
+					{
+						Name: "Name",
+						Value: func(row *downballotapi.UserGroup) any {
+							return row.Name
+						},
+						To: func(row *downballotapi.UserGroup) string {
+							return fmt.Sprintf("/organization/%s/group/%s", c.organizationID, row.ID)
+						},
+					},
+					{
+						Name: "Owner",
+						Value: func(row *downballotapi.UserGroup) any {
+							return row.Owner
+						},
+					},
+				}).
+				Action(myui.TableAction{
+					Name: "Add to group",
+					Icon: "plus",
+					Function: func(ctx app.Context) {
+						c.addUserToGroupDialog.Open(ctx)
+					},
+				}).
+				RowAction(myui.RowAction[*downballotapi.UserGroup]{
+					Name: "Remove",
+					Icon: "trash",
+					Function: func(ctx app.Context, row *downballotapi.UserGroup) {
+						ctx.PreventUpdate()
+
+						result := app.Window().Call("confirm", "Are you sure you want to remove this user from this group?")
+						slog.InfoContext(ctx.Context, "OrganizationIDUserIDPage: Delete button clicked", "result", result.Bool())
+						if !result.Bool() {
+							slog.InfoContext(ctx.Context, "OrganizationIDUserIDPage: Delete button clicked: User cancelled", "result", result.Bool())
+							return
+						}
+
+						ctx.Async(func() {
+							err := api.Do(ctx, http.MethodDelete, "/api/v1/organization/"+c.organizationID+"/group/"+row.ID+"/user/"+c.userID, nil, nil)
+							if err != nil {
+								slog.ErrorContext(ctx.Context, "Could not remove user from group", "err", err)
+								return
+							}
+
+							c.Reload(ctx)
+						})
+					},
+				}),
+			&c.addUserToGroupDialog,
 		)
 }
