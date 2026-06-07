ec5e5683523fb90e71783984d1f69cd4e43b4836
Douglas Danger Manley <doug.manley@gmail.com>
2026-06-07 12:31:07 -0400

    Refactor to use form

--- component/page/organization-id-user-new-page.go
+++ component/page/organization-id-user-new-page.go
@@ -39,9 +39,7 @@ func (c *OrganizationIDUserNewPage) Render() app.UI {
 	slog.InfoContext(context.TODO(), "OrganizationIDUserNewPage: Render")
 
 	return c.EmbeddedPage.Wrap(
-		app.Div().
-			Style("display", "flex").
-			Style("flex-direction", "column").
+		myui.Form().
 			Body(
 				myui.Input[string]().
 					Label("E-mail Address").
@@ -49,25 +47,24 @@ func (c *OrganizationIDUserNewPage) Render() app.UI {
 				myui.Input[bool]().
 					Label("Owner").
 					Bind(&c.Owner),
-				app.Div().Body(
-					myui.Button().
-						Label("Add User To Organization").
-						On("click", func(ctx app.Context, e app.Event) {
-							ctx.Async(func() {
-								var input downballotapi.AddUserToOrganizationRequest
-								input.Username = c.EmailAddress
-								input.Owner = c.Owner
-								var output downballotapi.AddUserToOrganizationResponse
-								err := api.Do(ctx, http.MethodPost, "/api/v1/organization/"+c.organizationID+"/user", input, &output)
-								if err != nil {
-									slog.ErrorContext(ctx.Context, "Could not add user to organization", "err", err)
-									return
-								}
-								slog.InfoContext(ctx.Context, "OrganizationIDUserNewPage: Create button clicked: Navigating to user page")
-								ctx.Navigate(fmt.Sprintf("/organization/%s/user/%s", c.organizationID, output.UserID))
-							})
-						}),
-				),
-			),
+			).
+			SubmitLabel("Add User To Organization").
+			SubmitFunction(func(ctx app.Context) {
+				ctx.PreventUpdate()
+
+				ctx.Async(func() {
+					var input downballotapi.AddUserToOrganizationRequest
+					input.Username = c.EmailAddress
+					input.Owner = c.Owner
+					var output downballotapi.AddUserToOrganizationResponse
+					err := api.Do(ctx, http.MethodPost, "/api/v1/organization/"+c.organizationID+"/user", input, &output)
+					if err != nil {
+						slog.ErrorContext(ctx.Context, "Could not add user to organization", "err", err)
+						return
+					}
+					slog.InfoContext(ctx.Context, "OrganizationIDUserNewPage: Create button clicked: Navigating to user page")
+					ctx.Navigate(fmt.Sprintf("/organization/%s/user/%s", c.organizationID, output.UserID))
+				})
+			}),
 	)
 }
