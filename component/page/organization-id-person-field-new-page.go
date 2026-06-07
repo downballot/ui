8301526214d6ac0d03143ce0fc9df303c208d90b
Douglas Danger Manley <doug.manley@gmail.com>
2026-06-07 12:23:02 -0400

    Refactor to use form

--- component/page/organization-id-person-field-new-page.go
+++ component/page/organization-id-person-field-new-page.go
@@ -54,49 +54,66 @@ func (c *OrganizationIDPersonFieldNewPage) Render() app.UI {
 	slog.InfoContext(context.TODO(), "OrganizationIDPersonFieldNewPage: Render")
 
 	return c.EmbeddedPage.Wrap(
-		myui.Input[string]().
-			Label("Name").
-			Type("text").
-			Bind(&c.Name),
-		myui.Input[string]().
-			Label("Type").
-			Type("text").
-			Bind(&c.Type),
-		myui.Input[bool]().
-			Label("Allow Empty").
-			Type("checkbox").
-			Bind(&c.AllowEmpty),
-		myui.Input[string]().
-			Label("Allowed Regex").
-			Type("text").
-			Bind(&c.AllowedRegex),
-		myui.Input[string]().
-			Label("Allowed Values").
-			Type("text").
-			Value(strings.Join(c.AllowedValues, ",")).
-			On("change", c.ValueTo(&c.AllowedValues)),
+		myui.Form().
+			Body(
+				myui.Input[string]().
+					Label("Name").
+					Type("text").
+					Bind(&c.Name),
+				myui.Select().
+					Label("Type").
+					AllowedValue(
+						myui.SelectOption{Label: "", Value: "", Disabled: true},
+					).
+					AllowedValue(
+						myui.SelectOption{Label: "Boolean", Value: string(downballotapi.PersonFieldDefinitionTypeBoolean)},
+						myui.SelectOption{Label: "Date", Value: string(downballotapi.PersonFieldDefinitionTypeDate)},
+						myui.SelectOption{Label: "Enum", Value: string(downballotapi.PersonFieldDefinitionTypeEnum)},
+						myui.SelectOption{Label: "Integer", Value: string(downballotapi.PersonFieldDefinitionTypeInteger)},
+						myui.SelectOption{Label: "Set", Value: string(downballotapi.PersonFieldDefinitionTypeSet)},
+						myui.SelectOption{Label: "String", Value: string(downballotapi.PersonFieldDefinitionTypeString)},
+					).
+					Bind(&c.Type),
+				myui.Input[bool]().
+					Label("Allow Empty").
+					Type("checkbox").
+					Bind(&c.AllowEmpty),
+				myui.Input[string]().
+					Label("Allowed Regex").
+					Type("text").
+					Bind(&c.AllowedRegex),
+				myui.Input[string]().
+					Label("Allowed Values").
+					Type("text").
+					Value(strings.Join(c.AllowedValues, ",")).
+					On("change", c.ValueTo(&c.AllowedValues)),
+			).
+			SubmitLabel("Create").
+			SubmitFunction(func(ctx app.Context) {
+				ctx.PreventUpdate()
+
+				ctx.Async(func() {
+					input := downballotapi.CreatePersonFieldRequest{
+						Name:          c.Name,
+						Type:          downballotapi.PersonFieldDefinitionType(c.Type),
+						AllowEmpty:    c.AllowEmpty,
+						AllowedRegex:  c.AllowedRegex,
+						AllowedValues: c.AllowedValues,
+					}
+					var output downballotapi.CreatePersonFieldResponse
+					err := api.Do(ctx, http.MethodPost, "/api/v1/organization/"+c.organizationID+"/person-field", input, &output)
+					if err != nil {
+						slog.ErrorContext(ctx.Context, "Could not create person field", "err", err)
+						return
+					}
+					slog.InfoContext(ctx.Context, "OrganizationIDPersonFieldNewPage: Create button clicked: Navigating to person field page", "person_field_id", output.PersonField.ID)
+					ctx.Navigate(fmt.Sprintf("/organization/%s/person-field/%s", c.organizationID, output.PersonField.ID))
+				})
+			}),
 		app.If(c.Error != "", func() app.UI {
 			return app.Div().Body(
 				app.Span().Text(c.Error),
 			)
 		}),
-		myui.Button().
-			Label("Create").
-			On("click", func(ctx app.Context, e app.Event) {
-				var input downballotapi.CreatePersonFieldRequest
-				input.Name = c.Name
-				input.Type = downballotapi.PersonFieldDefinitionType(c.Type)
-				input.AllowEmpty = c.AllowEmpty
-				input.AllowedRegex = c.AllowedRegex
-				input.AllowedValues = c.AllowedValues
-				var output downballotapi.CreatePersonFieldResponse
-				err := api.Do(ctx, http.MethodPost, "/api/v1/organization/"+c.organizationID+"/person-field", input, &output)
-				if err != nil {
-					slog.ErrorContext(ctx.Context, "Could not create person field", "err", err)
-					return
-				}
-				slog.InfoContext(ctx.Context, "OrganizationIDPersonFieldNewPage: Create button clicked: Navigating to person field page", "person_field_id", output.PersonField.ID)
-				ctx.Navigate(fmt.Sprintf("/organization/%s/person-field/%s", c.organizationID, output.PersonField.ID))
-			}),
 	)
 }
