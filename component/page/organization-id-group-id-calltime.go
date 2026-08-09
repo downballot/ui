148610e1cf1bb350270257ce5d178fcecf0b6e61
Douglas Danger Manley <doug.manley@gmail.com>
2026-08-09 11:52:58 -0400

    Only show buttons if we've clicked

--- component/page/organization-id-group-id-calltime.go
+++ component/page/organization-id-group-id-calltime.go
@@ -44,6 +44,8 @@ type OrganizationIDGroupIDCalltimePage struct {
 	PersonsTableVisibleColumns []string
 
 	filterOpen bool
+
+	lastCalledVoterID string
 }
 
 var _ app.Navigator = (*OrganizationIDGroupIDCalltimePage)(nil)
@@ -521,7 +523,10 @@ func (c *OrganizationIDGroupIDCalltimePage) Render() app.UI {
 					blazar.Button().
 						Label("Call").
 						Icon(component.IconPhone).
-						To("tel:+1"+strings.ReplaceAll(person.Fields["phone_number"], "-", "")),
+						To("tel:+1"+strings.ReplaceAll(person.Fields["phone_number"], "-", "")).
+						On("click", func(ctx app.Context, e app.Event) {
+							c.lastCalledVoterID = person.VoterID
+						}),
 					app.Div().
 						Style("font-size", "1.2em").
 						Style("margin-top", "auto").
@@ -529,107 +534,109 @@ func (c *OrganizationIDGroupIDCalltimePage) Render() app.UI {
 						Text(person.Fields["phone_number"]),
 				),
 			app.Hr(),
-			blazar.Form().
-				Class("no-print").
-				Spacer(false).
-				Action(
-					blazar.FormAction{
-						Name:   "Edit Person",
-						Icon:   component.IconExternalLink,
-						To:     fmt.Sprintf("/organization/%s/person/%s", c.organizationID, person.VoterID),
-						Target: "_blank",
-					},
-					blazar.FormAction{
-						Name:            "Bad Number",
-						Icon:            component.IconDelete,
-						BackgroundColor: "red",
-						Function: func(ctx app.Context) {
-							result := app.Window().Call("confirm", "Are you sure you want to mark this as a bad number?")
-							slog.InfoContext(ctx.Context, "OrganizationIDGroupIDCalltimePage: Bad Number button clicked", "result", result.Bool())
-							if !result.Bool() {
-								slog.InfoContext(ctx.Context, "OrganizationIDGroupIDCalltimePage: Bad number button clicked: User cancelled", "result", result.Bool())
-								return
-							}
-
-							phoneNumber := person.Fields["phone_number"]
-							if phoneNumber != "" {
-								queryParameters := url.Values{}
-								queryParameters.Set("filter", "phone_number = '"+phoneNumber+"'")
-								queryParameters.Set("limit", fmt.Sprintf("%d", 100))
-								var output downballotapi.ListPersonsResponse
-								err := api.Do(ctx, http.MethodGet, "/api/v1/organization/"+c.organizationID+"/group/"+c.groupID+"/person?"+queryParameters.Encode(), nil, &output)
-								if err != nil {
-									slog.ErrorContext(ctx.Context, "OrganizationIDGroupIDCalltimePage: search: Could not get persons", "err", err)
+			app.If(c.lastCalledVoterID == person.VoterID, func() app.UI {
+				return blazar.Form().
+					Class("no-print").
+					Spacer(false).
+					Action(
+						blazar.FormAction{
+							Name:   "Edit Person",
+							Icon:   component.IconExternalLink,
+							To:     fmt.Sprintf("/organization/%s/person/%s", c.organizationID, person.VoterID),
+							Target: "_blank",
+						},
+						blazar.FormAction{
+							Name:            "Bad Number",
+							Icon:            component.IconDelete,
+							BackgroundColor: "red",
+							Function: func(ctx app.Context) {
+								result := app.Window().Call("confirm", "Are you sure you want to mark this as a bad number?")
+								slog.InfoContext(ctx.Context, "OrganizationIDGroupIDCalltimePage: Bad Number button clicked", "result", result.Bool())
+								if !result.Bool() {
+									slog.InfoContext(ctx.Context, "OrganizationIDGroupIDCalltimePage: Bad number button clicked: User cancelled", "result", result.Bool())
 									return
 								}
 
-								var voterIDs []string
-								for _, person := range output.Persons {
-									voterIDs = append(voterIDs, person.VoterID)
-								}
-
-								if len(voterIDs) > 0 {
-									input := downballotapi.PostPersonUpdateRequest{
-										VoterIDs: voterIDs,
-										Fields: map[string]*string{
-											"phone_number": nil,
-										},
-									}
-									var output downballotapi.PostPersonUpdateResponse
-									err := api.Do(ctx, http.MethodPost, "/api/v1/organization/"+c.organizationID+"/person/update", input, &output)
+								phoneNumber := person.Fields["phone_number"]
+								if phoneNumber != "" {
+									queryParameters := url.Values{}
+									queryParameters.Set("filter", "phone_number = '"+phoneNumber+"'")
+									queryParameters.Set("limit", fmt.Sprintf("%d", 100))
+									var output downballotapi.ListPersonsResponse
+									err := api.Do(ctx, http.MethodGet, "/api/v1/organization/"+c.organizationID+"/group/"+c.groupID+"/person?"+queryParameters.Encode(), nil, &output)
 									if err != nil {
-										slog.ErrorContext(ctx.Context, "Could not update persons", "err", err)
+										slog.ErrorContext(ctx.Context, "OrganizationIDGroupIDCalltimePage: search: Could not get persons", "err", err)
 										return
 									}
-								}
-							}
 
-							c.search(ctx)
-						},
-					},
-					blazar.FormAction{
-						Name:            "Called",
-						Icon:            component.IconDone,
-						BackgroundColor: "green",
-						Function: func(ctx app.Context) {
-							phoneNumber := person.Fields["phone_number"]
-							if phoneNumber != "" {
-								queryParameters := url.Values{}
-								queryParameters.Set("filter", "phone_number = '"+phoneNumber+"'")
-								queryParameters.Set("limit", fmt.Sprintf("%d", 100))
-								var output downballotapi.ListPersonsResponse
-								err := api.Do(ctx, http.MethodGet, "/api/v1/organization/"+c.organizationID+"/group/"+c.groupID+"/person?"+queryParameters.Encode(), nil, &output)
-								if err != nil {
-									slog.ErrorContext(ctx.Context, "OrganizationIDGroupIDCalltimePage: search: Could not get persons", "err", err)
-									return
-								}
-
-								var voterIDs []string
-								for _, person := range output.Persons {
-									voterIDs = append(voterIDs, person.VoterID)
-								}
-
-								if len(voterIDs) > 0 {
-									dateCalled := time.Now().Format("2006-01-02")
-									input := downballotapi.PostPersonUpdateRequest{
-										VoterIDs: voterIDs,
-										Fields: map[string]*string{
-											"candidate.date_called": &dateCalled,
-										},
+									var voterIDs []string
+									for _, person := range output.Persons {
+										voterIDs = append(voterIDs, person.VoterID)
 									}
-									var output downballotapi.PostPersonUpdateResponse
-									err := api.Do(ctx, http.MethodPost, "/api/v1/organization/"+c.organizationID+"/person/update", input, &output)
+
+									if len(voterIDs) > 0 {
+										input := downballotapi.PostPersonUpdateRequest{
+											VoterIDs: voterIDs,
+											Fields: map[string]*string{
+												"phone_number": nil,
+											},
+										}
+										var output downballotapi.PostPersonUpdateResponse
+										err := api.Do(ctx, http.MethodPost, "/api/v1/organization/"+c.organizationID+"/person/update", input, &output)
+										if err != nil {
+											slog.ErrorContext(ctx.Context, "Could not update persons", "err", err)
+											return
+										}
+									}
+								}
+
+								c.search(ctx)
+							},
+						},
+						blazar.FormAction{
+							Name:            "Called",
+							Icon:            component.IconDone,
+							BackgroundColor: "green",
+							Function: func(ctx app.Context) {
+								phoneNumber := person.Fields["phone_number"]
+								if phoneNumber != "" {
+									queryParameters := url.Values{}
+									queryParameters.Set("filter", "phone_number = '"+phoneNumber+"'")
+									queryParameters.Set("limit", fmt.Sprintf("%d", 100))
+									var output downballotapi.ListPersonsResponse
+									err := api.Do(ctx, http.MethodGet, "/api/v1/organization/"+c.organizationID+"/group/"+c.groupID+"/person?"+queryParameters.Encode(), nil, &output)
 									if err != nil {
-										slog.ErrorContext(ctx.Context, "Could not update persons", "err", err)
+										slog.ErrorContext(ctx.Context, "OrganizationIDGroupIDCalltimePage: search: Could not get persons", "err", err)
 										return
 									}
-								}
-							}
 
-							c.search(ctx)
+									var voterIDs []string
+									for _, person := range output.Persons {
+										voterIDs = append(voterIDs, person.VoterID)
+									}
+
+									if len(voterIDs) > 0 {
+										dateCalled := time.Now().Format("2006-01-02")
+										input := downballotapi.PostPersonUpdateRequest{
+											VoterIDs: voterIDs,
+											Fields: map[string]*string{
+												"candidate.date_called": &dateCalled,
+											},
+										}
+										var output downballotapi.PostPersonUpdateResponse
+										err := api.Do(ctx, http.MethodPost, "/api/v1/organization/"+c.organizationID+"/person/update", input, &output)
+										if err != nil {
+											slog.ErrorContext(ctx.Context, "Could not update persons", "err", err)
+											return
+										}
+									}
+								}
+
+								c.search(ctx)
+							},
 						},
-					},
-				),
+					)
+			}),
 		)
 	}
 
